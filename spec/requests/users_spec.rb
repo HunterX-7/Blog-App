4bc12af88d722a87d90010f64c15deca41798bd0
HunterX-7 <fraquin7@gmail.com>
2023-04-11 19:45:30 -0500

    Add User Render template tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe 'Users', type: :request do
       get '/users/'
       expect(response).to have_http_status(:success)
     end
+
+    it 'returns render index template' do
+      get '/users/'
+      expect(response).to render_template(:index)
+    end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
       get '/users/:id'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns render show template' do
+      get '/users/:id'
+      expect(response).to render_template(:show)
     end
   end
 end
