64971672c94b74c7e2e3284718511ce6ce69b04f
HunterX-7 <fraquin7@gmail.com>
2023-04-11 19:45:39 -0500

    Add Post Render template tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe 'Posts', type: :request do
       get '/users/:id/posts/'
       expect(response).to have_http_status(:success)
     end
+
+    it 'returns render index template' do
+      get '/users/:id/posts/'
+      expect(response).to render_template(:index)
+    end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
       get '/users/:id/posts/:id/'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns render show template' do
+      get '/users/:id/posts/:id'
+      expect(response).to render_template(:show)
     end
   end
 end
