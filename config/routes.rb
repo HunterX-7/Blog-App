a08184035fb82f6970be636d3cbafd616bc1e0d5
HunterX-7 <fraquin7@gmail.com>
2023-04-14 16:14:37 -0500

    Update the routes to add comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  root "users#index"
+  resources :users do
+    resources :posts do
+      resources :comments, :likes
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
