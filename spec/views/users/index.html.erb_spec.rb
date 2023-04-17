e5178acf5486e6e0d567f56b16f02efd8cb91861
HunterX-7 <fraquin7@gmail.com>
2023-04-17 18:21:26 -0500

    Add integration tests for Users index page

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'users/index.html.erb', type: :feature do
       bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
       posts_counter: 0,
     )
-    second_user = User.create(
+    @second_user = User.create(
       name: 'Simon Bolivar',
       photo: 'https://es.wikipedia.org/wiki/Sim%C3%B3n_Bol%C3%ADvar#/media/Archivo:Sim%C3%B3n_Bol%C3%ADvar,_1825.jpg',
       bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
@@ -19,11 +19,24 @@ RSpec.describe 'users/index.html.erb', type: :feature do
   it 'shows the user info as it loads the page' do
     visit users_path
     expect(page).to have_content(@first_user.name)
-
     expect(page).to have_content(@second_user.name)
   end
 
   it 'shows the user profile picture as it loads the page' do
     visit users_path
-    expect(page).to have_css('img = @first_user.photo')
+    expect(page).to have_css("img[src='#{@first_user.photo}']")
+    expect(page).to have_css("img[src='#{@second_user.photo}']")
+  end
+
+  it 'shows the number of posts each user has written' do
+    visit users_path
+    expect(page).to have_content("Number of posts: #{@first_user.posts_counter}")
+    expect(page).to have_content("Number of posts: #{@second_user.posts_counter}")
+  end
+
+  it 'redirects me to the link of a selected user in the show page' do
+    visit users_path
+    click_link @first_user.name
+    expect(page).to have_current_path(user_path(@first_user))
+  end
 end
