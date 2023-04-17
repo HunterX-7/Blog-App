478e5219009e136f1351bbb5907b8cfd2b37a9d7
HunterX-7 <fraquin7@gmail.com>
2023-04-17 13:55:30 -0500

    Fix requests posts tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,37 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before(:each) do
+    @first_user = User.create(
+      name: 'first User',
+      photo: 'https://myphoto.com/photos/user/1',
+      bio: 'first User bio',
+      posts_counter: 0
+    )
+    @first_post = Post.create(
+      title: 'first Post', 
+      text: 'first Post text', 
+      comments_counter: 0, 
+      likes_counter: 0, 
+      author: @first_user
+    )
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/:id/posts/'
+      get user_posts_path(@first_user.id)
       expect(response).to have_http_status(:success)
     end
 
     it 'returns render index template' do
-      get '/users/:id/posts/'
+      get user_posts_path(@first_user.id)
       expect(response).to render_template(:index)
     end
 
     it 'body includes correct placeholder text' do
-      get '/users/:id/posts'
-      expect(response.body).to include('Posts#index')
+      get user_posts_path(@first_user.id)
+      expect(response.body).to include('first User')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/:id/posts/:id/'
+      get user_post_path(@first_user, @first_post)
       expect(response).to have_http_status(:success)
     end
 
     it 'returns render show template' do
-      get '/users/:id/posts/:id'
+      get user_post_path(@first_user, @first_post)
       expect(response).to render_template(:show)
     end
 
     it 'body includes correct placeholder text' do
-      get '/users/:id/posts/:id'
-      expect(response.body).to include('Posts#show')
+      get user_post_path(@first_user, @first_post)
+      expect(response.body).to include('first User')
     end
   end
 end
